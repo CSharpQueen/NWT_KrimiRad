ae1895d58d030d43b994d549642be065a03c1f5e
Faris Džafić <faris.dzafic@outlook.com>
2016-03-06 11:57:52 +0100

    Init3

--- KrimiRad/KrimiRad/App_Start/BundleConfig.cs
+++ KrimiRad/KrimiRad/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace KrimiRad {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                       "~/Scripts/angular.min.js"
+                     , "~/Scripts/angular-route.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
