071fdb353dfd0784d9c7bd98dd3e0fd7b0ad3f65
aidagranulo <agranulo1@etf.unsa.ba>
2016-03-29 17:26:25 +0200

    Kontroleri
    PoOpcini,PoTipuDjela

---
+++ KrimiRad/KrimiRadServis/Controllers/PrijavePoOpciniController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KrimiRadServis.Controllers
+{
+    public class PrijavePoOpciniController : ApiController
+    {
+
+       
+            private AppDbContext db = new AppDbContext();
+
+            [HttpGet]
+            public async Task<IHttpActionResult> Get(string opstina)
+            {
+
+                List<Prijava> prijave = db.Prijava.Where(p => p.Opstina.Contains(opstina)).ToList();
+                if (prijave == null) return Json("Ne postoje prijave za ovu opštinu");
+                return Json<List<Prijava>>(prijave);
+            }
+
+}
+}
---
+++ KrimiRad/KrimiRadServis/Controllers/PrijavePoTipuDjelaController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace KrimiRadServis.Controllers
+{
+    public class PrijavePoTipuDjelaController : ApiController
+    {
+
+        private AppDbContext db = new AppDbContext();
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+
+            List<Prijava> prijave = db.Prijava.Where(p => p.TipDjelaId.Contains(id)).ToList();
+            if (prijave == null) return Json("Ne postoje prijave za ovaj tip djela");
+            return Json<List<Prijava>>(prijave);
+        }
+    }
+}
