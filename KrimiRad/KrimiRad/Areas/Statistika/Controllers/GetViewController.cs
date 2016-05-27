4a5ba1db5f768db1a77ab7137255d0e19b5415df
sumejabotulja <sbotulja1@etf.unsa.ba>
2016-05-27 18:31:05 +0200

    Ispravke na prikazu detalja

--- KrimiRad/KrimiRad/Areas/Statistika/Controllers/GetViewController.cs
+++ KrimiRad/KrimiRad/Areas/Statistika/Controllers/GetViewController.cs
@@ -7,31 +7,7 @@ using System.Web.Mvc;
 namespace KrimiRad.Areas.Statistika.Controllers
 {
     public class GetViewController : Controller
-    {
-        // GET: Statistika/GetView/PoOpstiniITipuDjela
-        public PartialViewResult PoOpstiniITipuDjela()
-        {
-            return PartialView();
-        }
-
-        // GET: Statistika/GetView/PoOpstini
-        public PartialViewResult PoOpstini()
-        {
-            return PartialView();
-        }
-
-        // GET: Statistika/GetView/PoTipuDjela
-        public PartialViewResult PoTipuDjela()
-        {
-            return PartialView();
-        }
-
-        // GET: Statistika/GetView/PoDatumu
-        public PartialViewResult PoDatumu()
-        {
-            return PartialView();
-        }
-
+    {        
         public PartialViewResult BrojDjelaPoOpstinama() {
             return PartialView();
         }
@@ -48,5 +24,6 @@ namespace KrimiRad.Areas.Statistika.Controllers
         public PartialViewResult OmjerRjesenihUPeriodu() {
             return PartialView();
         }
+        
     }
 }
--- KrimiRad/KrimiRad/app/controller/statistika.js
+++ KrimiRad/KrimiRad/app/controller/statistika.js
@@ -86,61 +86,6 @@
             });
         }
 
-
-        //dobavljanje po opstini i tipu djela
-        $scope.PoOpstiniITipuDjela = function () {
-            $rootScope.loading = true;
-            statistikaService.dajPoOpstiniITipuDjela($scope.id, $scope.opstina).success(function (data) {
-
-                //stavljanje u chart
-
-            }).finally(function (data) {
-                $rootScope.loading = false;
-            });
-        };
-        //END dobavljanje po opstini i tipu djela
-
-
-        //dobavljanje po opstini 
-        $scope.PoOpstini = function () {
-            $rootScope.loading = true;
-            statistikaService.dajPoOpstini($scope.opstina).success(function (data) {
-
-                //stavljanje podataka u chart
-
-            }).finally(function (data) {
-                $rootScope.loading = false;
-            });
-        };
-        //END dobavljanje po opstini 
-
-
-        //dobavljanje po Tipu djela
-        $scope.PoTipuDjela = function () {
-            $rootScope.loading = true;
-            statistikaService.dajPoTipuDjela($scope.id).success(function (data) {
-
-                //stavljanje podataka u chart
-
-            }).finally(function (data) {
-                $rootScope.loading = false;
-            });
-        };
-        //END dobavljanje po tipu djela
-
-        //dobavljanje po datumu
-        $scope.PoDatumu = function () {
-            $rootScope.loading = true;
-            statistikaService.dajPoDatumu($scope.date).success(function (data) {
-
-                //stavljanje podataka u chart
-
-            }).finally(function (data) {
-                $rootScope.loading = false;
-            });
-        };
-        //END dobavljanje po datumu
-
         $scope.popup1 = false;
         $scope.popup2 = false;
 
